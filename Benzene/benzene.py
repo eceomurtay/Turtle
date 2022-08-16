b44087e66edfe709a3ed82d413750c5059d294b8
Ece Omurtay <ece.omurtay@gmail.com>
2022-08-16 10:51:56 +0300

    updated

--- Benzene/benzene.py
+++ Benzene/benzene.py
@@ -14,13 +14,10 @@ t.speed(15)
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-h = 0.0
-
 for i in range(360):
 
     c = colors[i%6]
     t.pencolor(c)
-    h += 0.01
     t.width(i//100 + 1)
     t.forward(i)
     t.left(59.5)
